9c3468e40b4b0dd3bf9a03f180de2462fcc313c4
willcharlesQA <w.charles@elsevier.com>
2019-07-29 18:12:44 +0100

    Fixed a few things

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("August 1, 2020 6:00:00").getTime();
+var countDownDate = new Date("August 1, 2020 24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
