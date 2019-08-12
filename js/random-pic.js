5b7b1f08597460ddff0ed0cd248b975681c29fd8
willcharlesQA <w.charles@elsevier.com>
2019-08-12 21:00:53 +0100

    Small changes after Maria's first review.

---
+++ js/random-pic.js
@@ -0,0 +1,9 @@
+var x = Math.random()
+
+if (x < 0.5) {
+  var icon-link = "icon-clover.png"
+} else {
+  var icon-link = "icon-daffodil.png"
+}
+
+document.getElementById("icon-pic").innerHTML = icon-link
