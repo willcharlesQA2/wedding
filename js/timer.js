8d8fac076648e83326018f6f18af948ac3b79755
willcharlesQA <w.charles@elsevier.com>
2020-05-17 12:09:21 +0100

    Change the date from August 1st 2020 to April 17th 2021.

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("August 1, 2020 24:00:00").getTime();
+var countDownDate = new Date("April 17, 2020 24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -14,7 +14,7 @@ var x = setInterval(function() {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
   // Output the result in an element with id="demo"
-  document.getElementById("timer").innerHTML = days + " days left till August 1st 2020!";
+  document.getElementById("timer").innerHTML = days + " days left till April 17th 2021!";
 
   // If the count down is over, write some text
   if (distance < 0) {
